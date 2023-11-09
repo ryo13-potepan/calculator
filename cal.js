9079d63aa39afca68d6423580cc5c186d92ccace
eno <ryo.enomo@gmail.com>
2023-11-10 00:58:23 +0900

    演算子と小数点の連続入力防止処理が未完了　質問

--- cal.js
+++ cal.js
@@ -1,25 +1,28 @@
+
 const result = document.getElementById("calDisplay");
+const operator = document.getElementById("opeButton");
+const number = document.getElementById("numButton");
 
-//（target）で、イベントが発生したエレメントを返す
+//（target）で、イベントが発生した要素を返す
 function clickButton(target) {
-  //console.log(target.innerHTML);
   let targetValue = target.innerHTML;
-  //result.value += targetValue;
   console.log(targetValue);
-
   if (targetValue == "AC") {
     result.value = "";
-  } else {
-    if (targetValue == "=") {
-      result.value = eval(result.value);
     } else {
-      result.value += targetValue;
+      if (targetValue == "=") {
+        result.value = eval(result.value);
+      } else {
+        if(result.value == "0") {
+          result.value = targetValue;
+        } else {
+          if (result.value.slice(-1) == `operator`){
+            const o = result.value.slice(0)
+            result.value = o + targetValue;
+          } else if(result.value.slice(-1) == `number`) {
+            result.value += targetValue}
+        }
+      }
     }
   }
-}
-//   if (buttonValue == "c") {
-//     result.innerHTML = "";
-//     console.log(buttonValue);
-//   } else {
-//     result.innerHTML = buttonValue;
-//   }
+  
