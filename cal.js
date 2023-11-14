e75a0975d3b47917b80b82f701477628855bf078
eno <ryo.enomo@gmail.com>
2023-11-14 13:02:54 +0900

    課題提出前

--- cal.js
+++ cal.js
@@ -7,10 +7,10 @@ function number (target){
   let targetValue = target.innerHTML;
   //確認用
   console.log(targetValue);
-  //ディスプレイの値が０なら、押下したnumberボタンの値に書き換える
+  //ディスプレイの値が０のみなら、押下したnumberボタンの値に書き換える
   if(result.value === "0"){
     result.value = targetValue;
-  //元が０でないなら、押下した値を連結していく
+  //元が０でないなら、押下したボタンの値を連結していく
   } else {
     result.value += targetValue;
   }
@@ -21,7 +21,8 @@ function ope(target) {
   let targetValue = target.innerHTML;
   //確認用
   console.log(targetValue);
-  //
+  //ディスプレイの値の右端が+,-,×,÷,小数点なら、returnで処理を終了する。
+  //そうでないなら、押下したボタンの値を連結して表示する。
   if(result.value.slice(-1) === "+"){
     return;
   } else if(result.value.slice(-1) === "-"){
@@ -37,6 +38,7 @@ function ope(target) {
   }
 }
 
+//電卓の計算処理
 function calc(target){
   let targetValue = target.innerHTML;
   console.log(targetValue);
